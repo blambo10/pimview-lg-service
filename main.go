7e93936d7ff36846a836558058601b2d9580c46c
Bryce Lamborne <blamb@live.com.au>
2023-02-07 21:55:22 +1100

    update dockerfile, receiving 401 on each call to webos within the docker container, need to sus

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+//todo: fix core library that tries to use non-commands to make calls to webos, or figure out why its getting 401 in the first place
+
 import (
 	"github.com/spf13/cobra"
 	"pimview.thelabshack.com/cmd"
